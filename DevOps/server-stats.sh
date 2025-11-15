814f06bbbb23832fe38d78369849320979381941
chris-doll <chris-doll@outlook.com>
2025-11-14 21:45:46 -0500

    added top5 processes for CPU and MEM%

--- DevOps/server-stats.sh
+++ DevOps/server-stats.sh
@@ -6,6 +6,7 @@ cpu_idle=$(top -bn1 | grep "Cpu(s)" | tr -s ' ' | awk '{print $8}' | sed 's/id,/
 #subtract idle from 100 to get usage and then print out
 cpu_usage=$(echo "100 - $cpu_idle" | bc)
 echo "Your CPU Usage is: $cpu_usage %"
+echo " "
 
 #get RAM info from free CLI tool, then extract and save the free, used, and total memory
 mem_free=$(free -h | grep "Mem:" | tr -s ' ' | awk '{print $4}' | sed 's/Gi//')
@@ -17,7 +18,7 @@ mem_fp=$(echo "scale=3;$mem_free / $mem_total * 100" | bc -l)
 mem_usp=$(echo "scale=3;$mem_used / $mem_total * 100" | bc -l)
 echo "Your RAM used is $mem_usp %"
 echo "Your RAM free is $mem_fp %"
-
+echo " "
 #get Disk usage stats from running df -h and save them
 disk_size=$(df -h | grep "/dev/sda" | tr -s ' ' | awk '{print $2}' | sed 's/G//')
 disk_used=$(df -h | grep "/dev/sda" | tr -s ' ' | awk '{print $3}' | sed 's/G//')
@@ -28,8 +29,17 @@ disk_frep=$(echo "scale=3;$disk_avail / $disk_size * 100" | bc -l)
 #print disk usage percentages
 echo "Your disk usage is $disk_usep %" 
 echo "Your disk free is $disk_frep %"
+echo " "
 
-
+#get top 5 processes by CPU usage and print out
+tfive_cpu=$(ps aux --sort=-%cpu | head -n 6)
+echo "Top 5 processes by CPU%"
+echo "$tfive_cpu"
+echo " "
+#get top 5 processes by MEM/RAM usage
+tfive_mem=$(ps aux --sort=-%mem | head -n 6)
+echo "Top 5 processes by MEM%"
+echo "$tfive_mem"
 
 
 

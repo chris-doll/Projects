482b28619b2411cf65acd0cb883557951e31e588
chris-doll <chris-doll@outlook.com>
2025-11-14 20:39:40 -0500

    added cpu used calc

--- DevOps/server-stats.sh
+++ DevOps/server-stats.sh
@@ -1,1 +1,6 @@
-#Hello
+#!/bin/bash
+
+cpu_idle=$(top -bn1 | grep "Cpu(s)" | tr -s ' ' | awk '{print $8}' | sed 's/id,//')
+cpu_usage=$(echo "100 - $cpu_idle" | bc)
+echo "Your CPU Usage is: $cpu_usage"
+

a6cc6e2a9cf9721e292ff9668d609f635e2f31d3
Christopher Doll <christopherdoll@Christophers-MacBook-Pro.local>
2025-11-16 17:14:31 -0500

    Added tf code to build ec2 on new vpc w/ necessary subnets, sg, etc

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -15,6 +15,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+
 # -----------------------------
 # Internet Gateway
 # -----------------------------
@@ -40,6 +41,7 @@ resource "aws_subnet" "public" {
   }
 }
 
+
 # -----------------------------
 # Public Route Table
 # -----------------------------
@@ -63,3 +65,72 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+/*
+# -----------------------------
+# Private Subnet
+# -----------------------------
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  map_public_ip_on_launch = false   # <-- Important!
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+*/
+
+# Create a security group that allows SSH
+resource "aws_security_group" "ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # open to world (not recommended for production)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0cae6d6fe6048ca2c" # Amazon Linux 2023 AMI 2023.9.20251110.1 x86_64 HVM kernel-6.1
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public.id # specify which subnet the ec2 will be created in
+#  subnet_id = aws_subnet.private.id 
+  key_name = "my-key-pair"   # <-- must exist in AWS EC2 prior to running this (EC2 > Key Pairs)
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
